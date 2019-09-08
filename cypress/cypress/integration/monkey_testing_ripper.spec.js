6b2d470569eb2f545dad68e01eace42286d316ad
victor <vgarzom@gmail.com>
2019-09-08 15:56:40 -0500

    modifies cypress test to run all the monkeys

--- cypress/cypress/integration/monkey_testing_ripper.spec.js
+++ cypress/cypress/integration/monkey_testing_ripper.spec.js
@@ -1,28 +1,37 @@
+var monkeysLeft = 10;
 describe('Los estudiantes under monkeys', function () {
   it('visits los estudiantes and survives monkeys', function () {
     cy.visit('https://losestudiantes.co');
     cy.contains('Cerrar').click();
     cy.wait(1000);
-    randomClick(10);
+    randomClick();
   })
 })
-function randomClick(monkeysLeft) {
 
-  function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min)) + min;
-  };
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+};
 
-  var monkeysLeft = monkeysLeft;
-  if (monkeysLeft > 0) {
-    cy.get('a').then($links => {
-      var randomLink = $links.get(getRandomInt(0, $links.length));
+function randomClick() {
+  console.log("Random click called", monkeysLeft);
+  monkeysLeft = monkeysLeft - 1;
+  if (monkeysLeft >= 0) {
+    cy.get('a').then((links) => {
+      console.log("links found", links.length);
+      var randomLink = links.get(getRandomInt(0, links.length));
       if (!Cypress.dom.isHidden(randomLink)) {
-        cy.wrap(randomLink).click({ force: true });
-        monkeysLeft = monkeysLeft - 1;
+        cy.wrap(randomLink).click({ force: true }).then(() => {
+          cy.wait(1000);
+          randomClick();
+        })
+      } else {
+        cy.wait(1000);
+          randomClick();
       }
-      setTimeout(randomClick, 1000, monkeysLeft);
     });
+  } else {
+    console.log("not more monkeys")
   }
 }
