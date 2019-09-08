1c8620bf66df76d03c9e44ebc4c4011fa6b98fec
victor <vgarzom@gmail.com>
2019-09-08 18:48:53 -0500

    limites clicker gremlins to buttons and links

--- gremlins/test/specs/gremlins-test.js
+++ gremlins/test/specs/gremlins-test.js
@@ -1,3 +1,15 @@
+
+function loadJquery(callback) {
+  var s = document.createElement('script');
+  s.src = '"https://code.jquery.com/jquery-3.4.1.js"';
+  if (s.addEventListener) {
+    s.addEventListener('load', callback, false);
+  } else if (s.readyState) {
+    s.onreadystatechange = callback;
+  }
+  document.body.appendChild(s);
+}
+
 function loadScript(callback) {
   var s = document.createElement('script');
   s.src = 'https://rawgithub.com/marmelab/gremlins.js/master/gremlins.min.js';
@@ -14,7 +26,15 @@ function unleashGremlins(ttl, callback) {
     horde.stop();
     callback();
   }
-  var horde = window.gremlins.createHorde();
+
+
+  var horde = window.gremlins.createHorde()
+    .gremlin(gremlins.species.clicker()
+      .clickTypes(['click'])
+      .canClick((element) => {
+        return element.nodeName === "BUTTON" || element.nodeName === "A";
+      }));
+      
   horde.seed(1234);
 
   horde.after(callback);
@@ -23,21 +43,21 @@ function unleashGremlins(ttl, callback) {
   horde.unleash();
 }
 
-describe('Monkey testing with gremlins ', function() {
+describe('Monkey testing with gremlins ', function () {
 
-  it('it should not raise any error', function() {
+  it('it should not raise any error', function () {
     browser.url('/');
     browser.click('button=Cerrar');
 
     browser.timeoutsAsyncScript(60000);
     browser.executeAsync(loadScript);
-
+    //browser.executeAsync(loadJquery);
     browser.timeoutsAsyncScript(60000);
     browser.executeAsync(unleashGremlins, 50000);
   });
 
-  afterAll(function() {
-    browser.log('browser').value.forEach(function(log) {
+  afterAll(function () {
+    browser.log('browser').value.forEach(function (log) {
       browser.logger.info(log.message.split(' ')[2]);
     });
   });
