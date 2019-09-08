361b02a6bb64bab2cb748e8cf8d505899df9beee
victor <vgarzom@gmail.com>
2019-09-08 18:00:55 -0500

    starts gremlins part

---
+++ gremlins/README.md
@@ -0,0 +1,13 @@
+Es proyecto utiliza Webdriver.io para inyectar el script de [gremlins.js](https://github.com/marmelab/gremlins.js/blob/master/README.md) a la página de [losestudiantes](https://losestudiantes.co) y realizar un ataque de Monkey testing.
+
+Para ejecutar las pruebas, clone este repositorio y descargue las dependencias con el siguiente comando en su terminal:
+
+```bash
+$ npm install
+```
+
+Luego para correrlas, también en su terminal:
+
+```bash
+$ npm test
+```
---
+++ gremlins/test/specs/gremlins-test.js
@@ -0,0 +1,45 @@
+function loadScript(callback) {
+  var s = document.createElement('script');
+  s.src = 'https://rawgithub.com/marmelab/gremlins.js/master/gremlins.min.js';
+  if (s.addEventListener) {
+    s.addEventListener('load', callback, false);
+  } else if (s.readyState) {
+    s.onreadystatechange = callback;
+  }
+  document.body.appendChild(s);
+}
+
+function unleashGremlins(ttl, callback) {
+  function stop() {
+    horde.stop();
+    callback();
+  }
+  var horde = window.gremlins.createHorde();
+  horde.seed(1234);
+
+  horde.after(callback);
+  window.onbeforeunload = stop;
+  setTimeout(stop, ttl);
+  horde.unleash();
+}
+
+describe('Monkey testing with gremlins ', function() {
+
+  it('it should not raise any error', function() {
+    browser.url('/');
+    browser.click('button=Cerrar');
+
+    browser.timeoutsAsyncScript(60000);
+    browser.executeAsync(loadScript);
+
+    browser.timeoutsAsyncScript(60000);
+    browser.executeAsync(unleashGremlins, 50000);
+  });
+
+  afterAll(function() {
+    browser.log('browser').value.forEach(function(log) {
+      browser.logger.info(log.message.split(' ')[2]);
+    });
+  });
+
+});
